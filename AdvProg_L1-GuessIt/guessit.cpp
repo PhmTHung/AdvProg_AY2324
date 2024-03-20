7dc6ec0877d4fdd8bf8b1c9f994af5fb10d3b180
Pham Hung <hungphamvp09@gmail.com>
2024-03-20 22:30:10 +0700

    .

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -88,12 +88,8 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if(isContinued=='Y')
-    {
-        result = true;
-    }
-    else if(isContinued=='N') result = false;
-    return result;
+    if(isContinued=='Y') return true;
+    else return false;
 }
 
 
