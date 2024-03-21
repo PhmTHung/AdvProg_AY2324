e3f6be4ae792364d12877aac4c3d6c143200d251
Pham Hung <hungphamvp09@gmail.com>
2024-03-21 07:54:10 +0700

    ,

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -55,9 +55,9 @@ bool isCharInWord(const char ch, const string& word)
     // TODO: return true if ch is in word else return false
     for(int i=0;i<word.length();i++)
     {
-        if(word[i]==ch) return true;
+        if(word[i]!=ch) return false;
     }
-    return false;
+    return true;
 }
 
 /***
